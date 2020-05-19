75446fd2d1f8cd8597b08b9f86afe1d4725e0979
maslamdevops <ec2-user@ip-172-31-47-12.us-east-2.compute.internal>
2020-05-19 12:45:07 +0000

    adding my shared lib

---
+++ vars/mystep.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+//This is the comment and will not be and can not be considered for compilation
+
+def stepCall(string name = 'aslamname' ){
+  echo "calling stepCall, $(name)"
+}
+
